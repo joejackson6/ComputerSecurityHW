29b32a668588a4928aaad620ea26be2be0509516
joejackson6 <123010521+joejackson6@users.noreply.github.com>
2025-02-27 12:59:20 -0500

    Create problem6.py

---
+++ problem6.py
@@ -0,0 +1,181 @@
+def apply_permutation(bitstr, perm_list):
+    return ''.join(bitstr[i - 1] for i in perm_list)
+
+def circular_left_shift(bitstr, shift_amt):
+    return bitstr[shift_amt:] + bitstr[:shift_amt]
+
+def xor_bin(a, b):
+    return ''.join('0' if a[i] == b[i] else '1' for i in range(len(a)))
+
+ip_map = [58, 50, 42, 34, 26, 18, 10, 2,
+          60, 52, 44, 36, 28, 20, 12, 4,
+          62, 54, 46, 38, 30, 22, 14, 6,
+          64, 56, 48, 40, 32, 24, 16, 8,
+          57, 49, 41, 33, 25, 17, 9, 1,
+          59, 51, 43, 35, 27, 19, 11, 3,
+          61, 53, 45, 37, 29, 21, 13, 5,
+          63, 55, 47, 39, 31, 23, 15, 7]
+
+fp_map = [40, 8, 48, 16, 56, 24, 64, 32,
+          39, 7, 47, 15, 55, 23, 63, 31,
+          38, 6, 46, 14, 54, 22, 62, 30,
+          37, 5, 45, 13, 53, 21, 61, 29,
+          36, 4, 44, 12, 52, 20, 60, 28,
+          35, 3, 43, 11, 51, 19, 59, 27,
+          34, 2, 42, 10, 50, 18, 58, 26,
+          33, 1, 41, 9, 49, 17, 57, 25]
+
+expansion_box = [32, 1, 2, 3, 4, 5,
+                 4, 5, 6, 7, 8, 9,
+                 8, 9, 10, 11, 12, 13,
+                 12, 13, 14, 15, 16, 17,
+                 16, 17, 18, 19, 20, 21,
+                 20, 21, 22, 23, 24, 25,
+                 24, 25, 26, 27, 28, 29,
+                 28, 29, 30, 31, 32, 1]
+
+straight_pbox = [16, 7, 20, 21,
+                 29, 12, 28, 17,
+                 1, 15, 23, 26,
+                 5, 18, 31, 10,
+                 2, 8, 24, 14,
+                 32, 27, 3, 9,
+                 19, 13, 30, 6,
+                 22, 11, 4, 25]
+
+s_boxes = [
+    [
+        [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+        [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+        [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+        [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+    ],
+    [
+        [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+        [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+        [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+        [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
+    ],
+    [
+        [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+        [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+        [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+        [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
+    ],
+    [
+        [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+        [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+        [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+        [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
+    ],
+    [
+        [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+        [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+        [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+        [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
+    ],
+    [
+        [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+        [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+        [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+        [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
+    ],
+    [
+        [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+        [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+        [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+        [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
+    ],
+    [
+        [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+        [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+        [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+        [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
+    ]
+]
+
+parity_drop = [57, 49, 41, 33, 25, 17, 9,
+               1, 58, 50, 42, 34, 26, 18,
+               10, 2, 59, 51, 43, 35, 27,
+               19, 11, 3, 60, 52, 44, 36,
+               63, 55, 47, 39, 31, 23, 15,
+               7, 62, 54, 46, 38, 30, 22,
+               14, 6, 61, 53, 45, 37, 29,
+               21, 13, 5, 28, 20, 12, 4]
+
+shift_schedule = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
+
+key_compress = [14, 17, 11, 24, 1, 5,
+                3, 28, 15, 6, 21, 10,
+                23, 19, 12, 4, 26, 8,
+                16, 7, 27, 20, 13, 2,
+                41, 52, 31, 37, 47, 55,
+                30, 40, 51, 45, 33, 48,
+                44, 49, 39, 56, 34, 53,
+                46, 42, 50, 36, 29, 32]
+
+def generate_decryption_round_keys(key_bin):
+    key_56 = apply_permutation(key_bin, parity_drop)
+    left_key = key_56[:28]
+    right_key = key_56[28:]
+    round_keys = []
+    print("Generating round keys:")
+    for i in range(16):
+        left_key = circular_left_shift(left_key, shift_schedule[i])
+        right_key = circular_left_shift(right_key, shift_schedule[i])
+        combined_key = left_key + right_key
+        round_key = apply_permutation(combined_key, key_compress)
+        round_keys.append(round_key)
+        print("Round", i+1, ":", round_key)
+    return round_keys[::-1]
+
+def des_decrypt_binary(cipher_bin, key_bin):
+    print("Ciphertext:", cipher_bin)
+    permuted_text = apply_permutation(cipher_bin, ip_map)
+    print("After initial permutation:", permuted_text)
+    left_part = permuted_text[:32]
+    right_part = permuted_text[32:]
+    print("L0:", left_part)
+    print("R0:", right_part)
+    decryption_keys = generate_decryption_round_keys(key_bin)
+    for round_num in range(16):
+        print("\n--- Round", round_num+1, "---")
+        expanded_right = apply_permutation(right_part, expansion_box)
+        print("Expanded R:", expanded_right)
+        xored = xor_bin(expanded_right, decryption_keys[round_num])
+        print("XOR with key:", xored)
+        sbox_result = ""
+        for j in range(8):
+            six_bits = xored[j*6:(j+1)*6]
+            row = int(six_bits[0] + six_bits[-1], 2)
+            col = int(six_bits[1:5], 2)
+            sbox_value = s_boxes[j][row][col]
+            sbox_result += format(sbox_value, '04b')
+        print("S-box output:", sbox_result)
+        permuted_sbox = apply_permutation(sbox_result, straight_pbox)
+        print("After P-box:", permuted_sbox)
+        new_right = xor_bin(left_part, permuted_sbox)
+        print("New R:", new_right)
+        left_part, right_part = right_part, new_right
+        print("New L:", left_part)
+    combined = right_part + left_part
+    print("\nBefore final permutation:", combined)
+    plain_bin = apply_permutation(combined, fp_map)
+    print("After final permutation:", plain_bin)
+    return plain_bin
+
+def binary_to_english(binary_str):
+    if len(binary_str) % 8 != 0:
+        raise ValueError("Binary string length must be a multiple of 8.")
+    english_text = ""
+    for i in range(0, len(binary_str), 8):
+        byte = binary_str[i:i+8]
+        english_text += chr(int(byte, 2))
+    return english_text
+
+if __name__ == "__main__":
+    binary_ciphertext = "1100101011101101101000100110010101011111101101110011100001110011"
+    binary_key = "0100110001001111010101100100010101000011010100110100111001000100"
+    decrypted_binary = des_decrypt_binary(binary_ciphertext, binary_key)
+    english_text = binary_to_english(decrypted_binary)
+    print("Decrypted text:", english_text)
